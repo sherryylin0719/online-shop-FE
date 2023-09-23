2f73987ed352fde3e1e006d2336c138e06e0ba1f
sherryylin0719 <sherryylin0719@gmail.com>
2023-09-23 19:37:00 +0900

    feat: add styles, header and navbar

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+// import styles from './Header.module.css'
+
+function Header () {
+  return (
+    <header>
+      <h1>My App</h1>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar () {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a to="/">Home</a>
+        </li>
+        <li>
+          <a to="/about">About</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
